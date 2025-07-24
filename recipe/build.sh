b6a1ed305b3d386f39e2dab1f1f935e0fd16e16d
mencian <joshua.zhuang@yahoo.com>
2025-07-24 14:58:05 -0500

    clean up recipe

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-
 set -o errexit -o pipefail
+
+sed -i.bak 's|v0.904.0|0.904.0|' META.json
+sed -i.bak 's|v0.904.0|0.904.0|' Makefile.PL
+rm -rf *.bak
 
 if [[ -f Build.PL ]]; then
     perl Build.PL
@@ -8,8 +11,8 @@ if [[ -f Build.PL ]]; then
     perl ./Build test
     perl ./Build install --installdirs vendor
 elif [[ -f Makefile.PL ]]; then
-    perl Makefile.PL INSTALLDIRS=vendor
-    make
+    perl Makefile.PL INSTALLDIRS=vendor NO_PACKLIST=1 NO_PERLLOCAL=1
+    make -j"${CPU_COUNT}"
     make test
     make install
 else
