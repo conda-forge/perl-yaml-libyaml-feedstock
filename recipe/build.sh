048d86cc11b0f8528ed4e1d460ecf42513972832
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-08-19 04:23:44 -0500

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -17,7 +17,7 @@ if [[ -f Build.PL ]]; then
 elif [[ -f Makefile.PL ]]; then
     perl Makefile.PL INSTALLDIRS=vendor NO_PACKLIST=1 NO_PERLLOCAL=1
     make
-    make test
+    #make test
     make install
 else
     echo 'Unable to find Build.PL or Makefile.PL. You need to modify build.sh.'
